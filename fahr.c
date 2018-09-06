843301e2d2e940ea6a9d3303403bbaae5de250f0
Allison Prestwich <21prestwich@stfrancishs.org>
2018-09-06 21:55:29 +0000

    init

---
+++ fahr.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**** printf Fahrenheit-Celcius table ****/
+// edit 1
+int main(void)
+{
+    int fahr;
+
+    for (fahr=0; fahr<=300; fahr = fahr+20)
+
+    {
+        printf("f - %3d c - %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+    }
+}
+
